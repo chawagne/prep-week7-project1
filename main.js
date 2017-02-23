33832fce3cfebad526e3256f1b366df7477a43cf
Chad Wagner <chawagne@gmail.com>
2017-02-22 22:20:53 -0700

    skeleton done

--- main.js
+++ main.js
@@ -4,14 +4,36 @@
     ** Week 7 - Project 1  **
 *** Ajax and JSON with OMDB ***
 
-This assignment is more open than the past assignments. 
-You are free to build a simple web page that interacts 
-with OMBD. It can simply display data within the HTML 
+This assignment is more open than the past assignments.
+You are free to build a simple web page that interacts
+with OMBD. It can simply display data within the HTML
 page or buttons and text fields can be added to interact
-with the data and update the page accordingly. 
+with the data and update the page accordingly.
 
-Have fun! 
- 
+Have fun!
+
 =======================================================
 */
 
+$(document).ready(function() {
+$("<h1>Movies</h1>").appendTo("main");
+
+
+var $xhr = $.getJSON('https://www.omdbapi.com/?t=Frozen');
+
+$xhr.done(function(data) {
+   if ($xhr.status !== 200) {
+       return;
+   }
+   console.log(data); });
+
+   $xhr.fail(function(err) {
+     console.log(err);
+ });
+
+
+
+
+
+
+});
